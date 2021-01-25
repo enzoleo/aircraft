188b617c5c2164ec4b84c7c02fda30878bc36f87
enzoleo <enzoliao95@gmail.com>
2021-01-25 14:28:00 +0800

    Add boundary check to hero plane

--- src/aircraft/game/Flying.java
+++ src/aircraft/game/Flying.java
@@ -10,16 +10,48 @@ import java.awt.image.BufferedImage;
 // implement abstract methods in different ways.
 public abstract class Flying {
   protected BufferedImage image;
-  protected Point2D.Double location = new Point2D.Double(); // The current position.
+
+  // The current position.
+  protected Point2D.Double location = new Point2D.Double();
   protected double speed = 0; // The speed of the flying object.
 
   // Each object should have a specific move policy.
   public abstract void move();
 
-  // The display method: as the name suggests, this method draw the image onto
-  // the graphics user interface, so the argument is necessary.
+  // The display method: as the name suggests, this method draw the image
+  // onto the graphics user interface, so the argument is necessary.
   public void display(Graphics graphics) {
     // Default implementation: show image on the gui.
     graphics.drawImage(image, (int)location.x, (int)location.y, null);
   }
+
+  // Check whether the current flying object is displayed inside the window.
+  // Usually an object should be restricted so that it will not move outside
+  // the boundary, or it should be directly deleted once it moves out of the
+  // horizon.
+  protected int boundCheck() {
+    double xmin = location.x; // Left boundary.
+    double xmax = location.x + image.getWidth(); // Right boundary.
+    double ymin = location.y; // Bottom boundary.W
+    double ymax = location.y + image.getHeight(); // Top boundary.
+
+    int xind = 0, yind = 0;
+    if (xmin < 0) xind = -1; else if (xmax > AircraftWar.WIDTH)  xind = 1;
+    if (ymin < 0) yind = -3; else if (ymax > AircraftWar.HEIGHT) yind = 3;
+
+    return xind + yind;
+  }
+
+  // An object should take specific actions once it moves out of bound.
+  // Bullets should be deleted, hero plane should be reverted to the valid
+  // position, supplies should rebound when hit the left/right border, etc.
+  // NOTE the meaning of argument: only 9 values are valid.
+  // -------------------------------------
+  // | -4 |           -3            | -2 |
+  // -------------------------------------
+  // | -1 |     Game Window (0)     |  1 |
+  // -------------------------------------
+  // |  2 |           -3            |  4 |
+  // -------------------------------------
+  protected abstract void reactOnceInvalid(int indicator);
 }
--- src/aircraft/game/plane/HeroPlane.java
+++ src/aircraft/game/plane/HeroPlane.java
@@ -2,6 +2,7 @@ package aircraft.game.plane;
 
 import java.awt.Point;
 
+import aircraft.game.AircraftWar;
 import aircraft.game.ImageLoader;
 
 public class HeroPlane extends Plane {
@@ -12,18 +13,66 @@ public class HeroPlane extends Plane {
 
   // Constructor.
   public HeroPlane() {
+    // Load the plane from the image directory.
     image = ImageLoader.readImg("aircraft/images/hero_plane.png");
-    location.x = 150.; location.y = 400.; // The initial position of the hero plane.
-    health = 20;
+    if (image.getWidth()  >= AircraftWar.WIDTH ||
+        image.getHeight() >= AircraftWar.HEIGHT)
+      throw new RuntimeException("The size of hero plane image is invalid");
+
+    // The initial position of the hero plane.
+    location.x = 150.; location.y = 400.;
+    if (boundCheck() != 0) // It must be inside the window.
+      throw new RuntimeException(
+          "The hero plane is not at a valid location.");
+    
+    health = 20; // The initial health point.
     speed = 2.0; // The initial speed.
   }
 
-  // Implement the abstract method of the base class. The move method is called
-  // everytime, but the plane can be stationary as long as the direction point is
-  // equal to a zero point, e.g. when the user inputs nothing.
+  // Implement the abstract method of the base class. The move method is
+  // called everytime, but the plane can be stationary as long as the
+  // direction is equal to a zero point, e.g. when the user inputs nothing.
   @Override
   public void move() {
     location.x += speed * direction.x;
     location.y += speed * direction.y;
+    
+    int indicator = boundCheck();
+    if (indicator != 0) reactOnceInvalid(indicator);
+  }
+
+  @Override
+  protected void reactOnceInvalid(int indicator) {
+    switch (indicator) {
+      case  1:
+        // Reset the location to the right boundary. 
+        location.x = AircraftWar.WIDTH - image.getWidth(); break;
+      case -2:
+        // Reset the location to the top right corner.
+        location.x = AircraftWar.WIDTH - image.getWidth();
+        location.y = 0; break;
+      case  4:
+        // Reset the location to the bottom right corner.
+        location.x = AircraftWar.WIDTH - image.getWidth();
+        location.y = AircraftWar.HEIGHT - image.getHeight(); break;
+      case -1:
+        // Reset the location to the left boundary.
+        location.x = 0; break;
+      case -4:
+        // Reset the location to the top left corner.
+        location.x = 0; location.y = 0; break;
+      case  2:
+        // Reset the location to the bottom left corner.
+        location.x = 0;
+        location.y = AircraftWar.HEIGHT - image.getHeight(); break;
+      case -3:
+        // Reset the location to the top boundary.
+        location.y = 0; break;
+      case  3:
+        // Reset the location to the bottom boundary.
+        location.y = AircraftWar.HEIGHT - image.getHeight(); break;
+      default:
+        throw new RuntimeException("Invalid indicator is specified.");
+    }
   }
 }
